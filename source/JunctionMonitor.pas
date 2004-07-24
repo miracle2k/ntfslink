57fc96d2242837dd4b8a4335db62d14f42f40cf2
Michael Elsdörfer <michael@elsdoerfer.info>
2004-07-24 17:59:12 +0000

    junction tracking now fully functional

--- source/JunctionMonitor.pas
+++ source/JunctionMonitor.pas
@@ -19,86 +19,195 @@ Known Issues:
 
 unit JunctionMonitor;
 
+// TODO [future] If a folder is deleted, check if there are junctions for
+// objects within subdirectories.
+
+// TODO [future] With time comin gand going the registry will be spammed
+// with old junction tracking entries, woth targets were deleted a long
+// time ago. Somehow somebody somewhere this should be cleaned up.
+
 interface
 
 uses
-  Windows, Classes;
+  Windows, Classes, SysUtils;
 
-function GetFolderLinkCount(Folder: string): Integer;
-procedure TrackJunction(Junction, Target: string);
+// Returns the number of links to a given directory; this function also makes
+// sure that any old, invalid or non-existant links are removed.
+// The valid links are returned via the second var-Paramter.
+function GetFolderLinkCount(Folder: string; var LinksAsString: string): Integer;
+
+// Call this whenever a junction is created; it will store the neccessary
+// tracking information in a stream, or the registry. 
+procedure TrackJunctionCreate(Junction, Target: string);
 
 implementation
 
 uses
-  JclNTFS, Global;
+  JclNTFS, JclRegistry, Global, ShellObjExtended;
 
-procedure RemoveInvalidTrackingEntries(Folder: string);
-begin
+function GetFolderLinkCount(Folder: string; var LinksAsString: string): Integer;
 
-end;
-
-procedure GetFolderLinks(Folder: string; Links: TStrings);
-
-  procedure ReadFromStream;
+  procedure RemoveInvalidTrackingEntries(Links: TStrings); overload;
   var
-    FindData: TFindStreamData;
+    i: integer;
   begin
-    if NtfsFindFirstStream(Folder, [], FindData) then
-    begin
-      repeat
-         if FindData.Name = 'ntfslink.junction.tracking' then begin
-//           FindData.StreamID
-         end;
-      until not NtfsFindNextStream(FindData);
-      NtfsFindStreamClose(FindData);
+    // Remove all links not existing
+    for i := Links.Count - 1 downto 0 do
+      if (not DirectoryExists(Links[i])) or
+         (not NtfsIsFolderMountPoint(RemoveBackslash(Links[i]))) or
+         (AnsiCompareFileName(RemoveBackslash(GetJPDestination(Links[i])),
+                              RemoveBackslash(Folder)) <> 0) then
+        Links.Delete(i);
+  end;
+
+  function ReadFromStream: Integer;
+  var
+    Handle: THandle;
+    LinkList: TStringList;
+    {BytesWritten: Cardinal;}
+    StrToRead, StrToWrite: string;
+  begin
+    // Assume zero links
+    Result := 0;
+
+    // Open the stream
+    Handle := CreateFile(PChar(RemoveBackslash(Folder) + ':' + NTFSLINK_TRACKING_STREAM),
+                         {FILE_READ_DATA, not defined in Windows.pas}$1, 
+                         0, nil, OPEN_EXISTING,
+                         FILE_FLAG_BACKUP_SEMANTICS or FILE_FLAG_OPEN_REPARSE_POINT,  
+                         0);
+    if Handle <> 0 then
+      try
+        // Read the whole content
+        SetString(StrToRead, nil, GetFileSize(Handle, nil));
+        FileRead(Handle, Pointer(StrToRead)^, Length(StrToRead));
+
+        // Load everything into a TStringList object
+        try
+          LinkList := TStringList.Create;
+          try
+            // Call the overloaded function, which will do the actual work
+            LinkList.Text := StrToRead;
+            RemoveInvalidTrackingEntries(LinkList);
+
+            // Add the found links to the var param used to return this info
+            LinksAsString := LinksAsString + LinkList.Text;
+
+            // Finally, write the up-to-date list back into the stream
+            SetFilePointer(Handle, 0, nil, FILE_BEGIN);
+            StrToWrite := LinkList.Text;
+            {BytesWritten := }FileWrite(Handle, Pointer(StrToWrite)^, Length(StrToWrite));
+            SetEndOfFile(Handle);
+
+            // Return the number of links left
+            Result := LinkList.Count;
+          finally
+            LinkList.Free;
+          end;
+        except
+        end;
+      finally
+        CloseHandle(Handle);
+      end;
+  end;
+
+  function ReadFromRegistry: Integer;
+  var
+    LinkList: TStringList;
+    i: integer;
+  begin
+    // Assume zero links
+    Result := 0;
+    
+    try
+      LinkList := TStringList.Create;
+      try
+        // Read list of links from registry
+        if RegGetValueNames(HKEY_LOCAL_MACHINE,
+                            NTFSLINK_TRACKINGDATA_KEY + Folder, LinkList) then
+        begin
+          // Remove all the invalid items from the list
+          RemoveInvalidTrackingEntries(LinkList);
+
+          // Return the number of links found
+          Result := LinkList.Count;
+          // Add the found links to the var param used to return this info
+          LinksAsString := LinksAsString + LinkList.Text;
+
+          // Make sure only the existing junctions are stored - delete
+          // everything, and than write the clean list
+          RegDeleteKeyTree(HKEY_LOCAL_MACHINE, NTFSLINK_TRACKINGDATA_KEY + Folder);
+          RegCreateKey(HKEY_LOCAL_MACHINE, NTFSLINK_TRACKINGDATA_KEY + Folder, '');
+          for i := 0 to LinkList.Count - 1 do
+            RegWriteString(HKEY_LOCAL_MACHINE, NTFSLINK_TRACKINGDATA_KEY + Folder, LinkList[i], '');
+        end;
+      finally
+        LinkList.Free;
+      end;
+    except
     end;
   end;
 
-  procedure ReadFromRegistry;
-  begin
-
-  end;
 
 begin
-  ReadFromStream;
-  ReadFromRegistry;
-
-    //go through list and remove invalid
-end;
-
-function GetFolderLinkCount(Folder: string): Integer;
-var
-  LinkList: TStringList;
-begin
-  Result := 0;
-  LinkList := TStringList.Create;
   try
-    GetFolderLinks(Folder, LinkList);
-    Result := LinkList.Count;
-  finally
-    LinkList.Free;
+    LinksAsString := '';
+    // Number is the sum of the entries found in data stream + from registry
+    Result := ReadFromStream + ReadFromRegistry;
+    // Remove the line break at the end, if existing
+    LinksAsString := Trim(LinksAsString);
+  except
+    Result := 0;
   end;
 end;
 
-procedure TrackJunction(Junction, Target: string);
+procedure TrackJunctionCreate(Junction, Target: string);
+
+  function NtfsStreamsSupported(const Volume: string): Boolean;
+  var
+    MCL, Flags: Cardinal;
+  const
+    FILE_NAMED_STREAMS = $00040000;
+  begin
+    Result := GetVolumeInformation(PChar(Volume), nil, 0, nil, MCL, Flags, nil, 0);
+    if Result then
+      Result := (Flags and FILE_NAMED_STREAMS) <> 0;
+  end;
+  
 var
   Handle: THandle;
-  BytesWritten: Cardinal;
+  {BytesWritten: Cardinal;}
   StrToWrite: string;
 begin
-  Handle := CreateFile(PChar(Target + ':' + NTFSLINK_TRACKING_STREAM),
-                       GENERIC_WRITE, // TODO fewer rights possible?
-                       0, nil, OPEN_ALWAYS,
-                       FILE_FLAG_BACKUP_SEMANTICS,  // TODO other flags that are needed?
-                       0);
-  if Handle <> 0 then
-    try
-      SetFilePointer(Handle, 0, nil, FILE_END);
-      StrToWrite := Junction + #13#10;
-      WriteFile(Handle, StrToWrite, Length(StrToWrite), BytesWritten, nil);
-    finally
-      CloseHandle(Handle);
-    end;
+  // Check if this the target path is prefixed with "\??\", if yes, remove it
+  if Copy(Target, 1, 4) = '\??\' then
+    Delete(Target, 1, 4);
+
+  // If the target file system supports streams, store information there
+  if NtfsStreamsSupported(ExtractFileDrive(Target) + '\') then
+  begin
+    // Open the file, append the new junction path, close
+    Handle := CreateFile(PChar(RemoveBackslash(Target) + ':' + NTFSLINK_TRACKING_STREAM),
+                         {FILE_WRITE_DATA, not defined in Windows.pas}$2,
+                         0, nil, OPEN_ALWAYS,
+                         FILE_FLAG_BACKUP_SEMANTICS or FILE_FLAG_OPEN_REPARSE_POINT,
+                         0);
+    if Handle <> 0 then
+      try
+        SetFilePointer(Handle, 0, nil, FILE_END);
+        StrToWrite := Junction + #13#10;
+        {BytesWritten := }FileWrite(Handle, Pointer(StrToWrite)^, Length(StrToWrite));
+      finally
+        CloseHandle(Handle);
+      end;
+  end
+  
+  // If streams are not supported, use the registry
+  else begin
+    RegCreateKey(HKEY_LOCAL_MACHINE, NTFSLINK_TRACKINGDATA_KEY + Target, '');
+    RegWriteString(HKEY_LOCAL_MACHINE, NTFSLINK_TRACKINGDATA_KEY + Target, Junction, '');
+  end;
 end;
 
 end.
+
